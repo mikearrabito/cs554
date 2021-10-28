428d6a9a2e1fd0f67cfa223dd1f4a8dd30241afe
Michael Arrabito <michael@mbp14inch2021.myfiosgateway.com>
2021-10-27 22:29:05 -0400

    Error handling

--- lab5/lab5-frontend/src/components/MainPage.js
+++ lab5/lab5-frontend/src/components/MainPage.js
@@ -4,29 +4,17 @@ import { useEffect, useState } from "react";
 import { useLazyQuery } from "@apollo/client";
 import { unsplashImages } from "../gql/queries";
 
-/*
-Should display a list of image post results from Unsplash.
-A user should be able to click a “Get More” button in order to perform another query to get more images from Unsplash.
-A user should be able to click on a “Add to Bin” button found on a particular image post to “bin” a post.
-Once a post has been added to the user’s bin, the “Add to bin” button should be toggled to “Remove from bin”
-Each image post should contain a description, image, and the original poster/author
-*/
-
 const MainPage = () => {
   const [images, setImages] = useState([]);
   const [page, setPage] = useState(1);
 
-  const addImages = (images) => {
-    setImages((i) => i.concat(images.unsplashImages));
-  };
-
-  const showError = () => {
-    // handle error
-  };
-
   const [getImages, { loading }] = useLazyQuery(unsplashImages, {
-    onCompleted: addImages,
-    onError: showError,
+    onCompleted: (images) => {
+      setImages((i) => i.concat(images.unsplashImages));
+    },
+    onError: () => {
+      alert("Error fetching page of images, try again");
+    },
   });
 
   useEffect(() => {
@@ -46,7 +34,11 @@ const MainPage = () => {
       <div>
         <ImageList images={images} allowBin />
         <button
-          onClick={() => setPage(page + 1)}
+          onClick={() => {
+            if (!loading) {
+              setPage(page + 1);
+            }
+          }}
           style={{ marginBottom: "20px" }}
         >
           Get More
