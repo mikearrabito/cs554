45d80157f4b3b463eb86bf8b9172ca651961f83e
Michael Arrabito <mikearrabito@gmail.com>
2021-12-21 11:17:18 -0500

    update

--- lab7/backend/app.ts
+++ lab7/backend/app.ts
@@ -1,8 +1,12 @@
 const { ApolloServer } = require("apollo-server");
-const typeDefs = require("./gqlSchema");
 const resolvers = require("./gqlResolvers");
+const { GraphQLFileLoader } = require("@graphql-tools/graphql-file-loader");
+const { loadSchema } = require("@graphql-tools/load");
 
 const startApollo = async () => {
+  const typeDefs = await loadSchema("schema.graphql", {
+    loaders: [new GraphQLFileLoader()],
+  });
   const server = new ApolloServer({ typeDefs, resolvers });
   const { url } = await server.listen();
   console.log(`Apollo server running at ${url}`);
